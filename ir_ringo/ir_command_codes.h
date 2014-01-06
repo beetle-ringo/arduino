3d9d8f8bcbf32c738e0fb6924152c04e1eaea58b
DmitryDzz <dzakhovds@gmail.com>
2014-01-06 14:59:58 +0400

    #4 many IR codes to each command

---
+++ ir_ringo/ir_command_codes.h
@@ -0,0 +1,18 @@
+// EN: Сommand codes received from IR remote. One command can be assigned to many codes.
+//     You can type codes from several IR remotes.
+// RU: Коды команд, принимаемых от ИК-пульта. Одна команда может вызываться несколькими
+//     кодами. Вы можете ввести коды от нескольких ИК-пультов.
+const long IR_COMMAND_FORWARD_CODES[] = { 527175, 2050, 2 };
+const long IR_COMMAND_BACKWARD_CODES[] = { 920391, 2056, 8 };
+const long IR_COMMAND_TURN_LEFT_CODES[] = { 789319, 2052, 4 };
+const long IR_COMMAND_TURN_RIGHT_CODES[] = { 658247, 2054, 6 };
+const long IR_COMMAND_FORWARD_LEFT_CODES[] = { 2887, 2049, 1 };
+const long IR_COMMAND_FORWARD_RIGHT_CODES[] = { 265031, 2051, 3 };
+const long IR_COMMAND_BACKWARD_LEFT_CODES[] = { 396103, 2055, 7 };
+const long IR_COMMAND_BACKWARD_RIGHT_CODES[] = { 68423, 2057, 9 };
+const long IR_COMMAND_STOP_CODES[] = { 133959, 2053, 5 };
+const long IR_COMMAND_VERY_SLOW_CODES[] = { 420679, 2088, 40 };
+const long IR_COMMAND_SLOW_CODES[] = { 617287, 2091, 43 };
+const long IR_COMMAND_FAST_CODES[] = { 92999, 2074, 26 };
+const long IR_COMMAND_VERY_FAST_CODES[] = { 944967, 2075, 27 };
+
--- ir_ringo/ir_ringo.ino
+++ ir_ringo/ir_ringo.ino
@@ -4,23 +4,9 @@
 //     https://github.com/shirriff/Arduino-IRremote
 #include "IRremote.h"
 
-#include <Servo.h>
+#include "ir_command_codes.h"
 
-// EN: Сommand codes received from IR remote.
-// RU: Коды команд, принимаемых от ИК-пульта.
-const long COMMAND_FORWARD = 527175;
-const long COMMAND_BACKWARD = 920391;
-const long COMMAND_TURN_LEFT = 789319;
-const long COMMAND_TURN_RIGHT = 658247;
-const long COMMAND_FORWARD_LEFT = 2887;
-const long COMMAND_FORWARD_RIGHT = 265031;
-const long COMMAND_BACKWARD_LEFT = 396103;
-const long COMMAND_BACKWARD_RIGHT = 68423;
-const long COMMAND_STOP = 133959;
-const long COMMAND_VERY_SLOW = 420679;
-const long COMMAND_SLOW = 617287;
-const long COMMAND_FAST = 92999;
-const long COMMAND_VERY_FAST = 944967;
+#include <Servo.h>
 
 // EN: Analog pin where IR detector is pluged in.
 // RU: Аналоговый вход контроллера, к которму подключен ИК-приёмник.
@@ -48,7 +34,7 @@ const long SIDE_SERVOS_FULL_AMPLITUDE = 30;
 const long SIDE_SERVOS_HALF_AMPLITUDE = 15;
 // EN: Amplitude of central servo.
 // RU: Амплитула центрального серводвигателя.
-const long CENTRAL_SERVO_AMPLITUDE = 15;
+const long CENTRAL_SERVO_AMPLITUDE = 20;
 
 // EN: Periods for different speeds.
 // RU: Периоды колебаний для различных скоростей.
@@ -128,6 +114,16 @@ int getAngle(long amplitude, long phaseMillis, float shiftAngle) {
   return (int)angle;
 }
 
+template<typename T,size_t N>
+boolean hasCode(T (&commandCodes)[N], long code) {
+  for (int i = 0; i < N; i++) {
+    if (commandCodes[i] == code) {
+      return true;
+    }
+  }
+  return false;
+}
+
 void loop() {
   long millisNow = millis();
   long millisPassed = millisNow - lastMillis;
@@ -156,81 +152,77 @@ void loop() {
   // EN: We can handle IR command if it is received and decoded successfully.
   // RU: Если команда успешно принята и декодирована, то мы можем её обрабатывать.
   if (irrecv.decode(&results)) {
-    switch (results.value) {
-      case COMMAND_FORWARD:
-      case COMMAND_FORWARD_LEFT:
-      case COMMAND_FORWARD_RIGHT:
-        attachServos();
-        isStopped = false;
-        angleShiftLeftServo = 0;
-        angleShiftRightServo = 0;
-        angleShiftCentralServo = PI/2;
+    
+    if (hasCode(IR_COMMAND_FORWARD_CODES, results.value) ||
+        hasCode(IR_COMMAND_FORWARD_LEFT_CODES, results.value) ||
+        hasCode(IR_COMMAND_FORWARD_RIGHT_CODES, results.value)) {
+    
+      attachServos();
+      isStopped = false;
+      angleShiftLeftServo = 0;
+      angleShiftRightServo = 0;
+      angleShiftCentralServo = PI/2;
         
-        amplitudeLeftServo = SIDE_SERVOS_FULL_AMPLITUDE;
-        amplitudeRightServo = SIDE_SERVOS_FULL_AMPLITUDE;
-        if (results.value == COMMAND_FORWARD_LEFT) amplitudeLeftServo = SIDE_SERVOS_HALF_AMPLITUDE;
-        else if (results.value == COMMAND_FORWARD_RIGHT) amplitudeRightServo = SIDE_SERVOS_HALF_AMPLITUDE;
-        
-        break;
-      case COMMAND_BACKWARD:
-      case COMMAND_BACKWARD_LEFT:
-      case COMMAND_BACKWARD_RIGHT:
-        attachServos();
-        isStopped = false;
-        angleShiftLeftServo = 0;
-        angleShiftRightServo = 0;
-        angleShiftCentralServo = -PI/2;
+      amplitudeLeftServo = SIDE_SERVOS_FULL_AMPLITUDE;
+      amplitudeRightServo = SIDE_SERVOS_FULL_AMPLITUDE;
+      if (hasCode(IR_COMMAND_FORWARD_LEFT_CODES, results.value)) {
+        amplitudeLeftServo = SIDE_SERVOS_HALF_AMPLITUDE;
+      } else if (hasCode(IR_COMMAND_FORWARD_RIGHT_CODES, results.value)) {
+        amplitudeRightServo = SIDE_SERVOS_HALF_AMPLITUDE;
+      }
+    } else if(hasCode(IR_COMMAND_BACKWARD_CODES, results.value) ||
+              hasCode(IR_COMMAND_BACKWARD_LEFT_CODES, results.value) ||
+              hasCode(IR_COMMAND_BACKWARD_RIGHT_CODES, results.value)) {
 
-        amplitudeLeftServo = SIDE_SERVOS_FULL_AMPLITUDE;
-        amplitudeRightServo = SIDE_SERVOS_FULL_AMPLITUDE;
-        if (results.value == COMMAND_BACKWARD_LEFT) amplitudeLeftServo = SIDE_SERVOS_HALF_AMPLITUDE;
-        else if (results.value == COMMAND_BACKWARD_RIGHT) amplitudeRightServo = SIDE_SERVOS_HALF_AMPLITUDE;
-        break;
-      case COMMAND_TURN_LEFT:
-        attachServos();
-        isStopped = false;
-        angleShiftLeftServo = 0;
-        angleShiftRightServo = PI;
-        angleShiftCentralServo = -PI/2;
-        amplitudeLeftServo = SIDE_SERVOS_FULL_AMPLITUDE;
-        amplitudeRightServo = SIDE_SERVOS_FULL_AMPLITUDE;
-        break;
-      case COMMAND_TURN_RIGHT:
-        attachServos();
-        isStopped = false;
-        angleShiftLeftServo = 0;
-        angleShiftRightServo = PI;
-        angleShiftCentralServo = PI/2;
-        amplitudeLeftServo = SIDE_SERVOS_FULL_AMPLITUDE;
-        amplitudeRightServo = SIDE_SERVOS_FULL_AMPLITUDE;
-        break;
-      case COMMAND_STOP:
-        attachServos();
-        isStopped = true;
-        angleShiftLeftServo = 0;
-        angleShiftRightServo = 0;
-        angleShiftCentralServo = 0;
-        amplitudeLeftServo = SIDE_SERVOS_FULL_AMPLITUDE;
-        amplitudeRightServo = SIDE_SERVOS_FULL_AMPLITUDE;
-        break;
-      case COMMAND_VERY_SLOW:
-        // EN: globalPhase correction to save servo positions when changing period.
-        // RU: Корректировка globalPhase чтобы сохранить положение серв при смене периода колебаний ног.
-        globalPhase = globalPhase * STEP_PERIOD_VERY_SLOW / stepPeriod;
-        stepPeriod = STEP_PERIOD_VERY_SLOW;
-        break;
-      case COMMAND_SLOW:
-        globalPhase = globalPhase * STEP_PERIOD_SLOW / stepPeriod;
-        stepPeriod = STEP_PERIOD_SLOW;
-        break;
-      case COMMAND_FAST:
-        globalPhase = globalPhase * STEP_PERIOD_FAST / stepPeriod;
-        stepPeriod = STEP_PERIOD_FAST;
-        break;
-      case COMMAND_VERY_FAST:
-        globalPhase = globalPhase * STEP_PERIOD_VERY_FAST / stepPeriod;
-        stepPeriod = STEP_PERIOD_VERY_FAST;
-        break;
+      attachServos();
+      isStopped = false;
+      angleShiftLeftServo = 0;
+      angleShiftRightServo = 0;
+      angleShiftCentralServo = -PI/2;
+
+      amplitudeLeftServo = SIDE_SERVOS_FULL_AMPLITUDE;
+      amplitudeRightServo = SIDE_SERVOS_FULL_AMPLITUDE;
+      if (hasCode(IR_COMMAND_BACKWARD_LEFT_CODES, results.value)) {
+        amplitudeLeftServo = SIDE_SERVOS_HALF_AMPLITUDE;
+      } else if (hasCode(IR_COMMAND_BACKWARD_RIGHT_CODES, results.value)) amplitudeRightServo = SIDE_SERVOS_HALF_AMPLITUDE;
+    } else if (hasCode(IR_COMMAND_TURN_LEFT_CODES, results.value)) {
+      attachServos();
+      isStopped = false;
+      angleShiftLeftServo = 0;
+      angleShiftRightServo = PI;
+      angleShiftCentralServo = -PI/2;
+      amplitudeLeftServo = SIDE_SERVOS_FULL_AMPLITUDE;
+      amplitudeRightServo = SIDE_SERVOS_FULL_AMPLITUDE;
+    } else if (hasCode(IR_COMMAND_TURN_RIGHT_CODES, results.value)) {
+      attachServos();
+      isStopped = false;
+      angleShiftLeftServo = 0;
+      angleShiftRightServo = PI;
+      angleShiftCentralServo = PI/2;
+      amplitudeLeftServo = SIDE_SERVOS_FULL_AMPLITUDE;
+      amplitudeRightServo = SIDE_SERVOS_FULL_AMPLITUDE;
+    } else if (hasCode(IR_COMMAND_STOP_CODES, results.value)) {
+      attachServos();
+      isStopped = true;
+      angleShiftLeftServo = 0;
+      angleShiftRightServo = 0;
+      angleShiftCentralServo = 0;
+      amplitudeLeftServo = SIDE_SERVOS_FULL_AMPLITUDE;
+      amplitudeRightServo = SIDE_SERVOS_FULL_AMPLITUDE;
+    } else if (hasCode(IR_COMMAND_VERY_SLOW_CODES, results.value)) {
+      // EN: globalPhase correction to save servo positions when changing period.
+      // RU: Корректировка globalPhase чтобы сохранить положение серв при смене периода колебаний ног.
+      globalPhase = globalPhase * STEP_PERIOD_VERY_SLOW / stepPeriod;
+      stepPeriod = STEP_PERIOD_VERY_SLOW;
+    } else if (hasCode(IR_COMMAND_SLOW_CODES, results.value)) {
+      globalPhase = globalPhase * STEP_PERIOD_SLOW / stepPeriod;
+      stepPeriod = STEP_PERIOD_SLOW;
+    } else if (hasCode(IR_COMMAND_FAST_CODES, results.value)) {
+      globalPhase = globalPhase * STEP_PERIOD_FAST / stepPeriod;
+      stepPeriod = STEP_PERIOD_FAST;
+    } else if (hasCode(IR_COMMAND_VERY_FAST_CODES, results.value)) {
+      globalPhase = globalPhase * STEP_PERIOD_VERY_FAST / stepPeriod;
+      stepPeriod = STEP_PERIOD_VERY_FAST;
     }
     // EN: Once a code has been decoded, the resume() method must be called to resume receiving codes.
     // RU: После декодирования кода для продолжения приёма должен вызаваться метод resume().
